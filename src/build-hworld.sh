b069e922cb590f2f8be1ea1c2df5680ad25a07bf
EC2 Default User <ec2-user@ip-10-230-17-167.us-west-2.compute.internal>
2020-01-07 12:31:46 +0000

    copy output to build folder

--- src/build-hworld.sh
+++ src/build-hworld.sh
@@ -2,4 +2,6 @@
 echo "building hworld in $1"
 g++ hworld.cpp -o hworldoutput_1 -x 'none'
 tar -czvf output.tar.gz hworldoutput_1
+mkdir ../build
+cp ./output.tar.gz ../build/
 echo "build completed"
